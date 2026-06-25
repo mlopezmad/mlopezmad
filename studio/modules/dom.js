549a6e9da43268e7acb3b8ab9309fac3f5f0cfb1
mlopezmad <65028173+mlopezmad@users.noreply.github.com>
2026-06-25 16:12:59 +0200

    Crear dom.js

---
+++ studio/modules/dom.js
@@ -0,0 +1,51 @@
+import { qs } from "./utils.js";
+
+export const dom = {
+    login: qs("login"),
+    dashboard: qs("dashboard"),
+    panel: qs("panel"),
+    collectionPanel: qs("collectionPanel"),
+    collectionManager: qs("collectionManager"),
+    success: qs("success"),
+    collectionSuccess: qs("collectionSuccess"),
+
+    passwordInput: qs("password"),
+    loginBtn: qs("loginBtn"),
+    loginStatus: qs("loginStatus"),
+
+    newPostBtn: qs("newPostBtn"),
+    newCollectionBtn: qs("newCollectionBtn"),
+    backBtn: qs("backBtn"),
+    backFromCollectionBtn: qs("backFromCollectionBtn"),
+
+    collectionSelect: qs("collection"),
+    filesInput: qs("files"),
+    preview: qs("preview"),
+    fileStatus: qs("fileStatus"),
+    publishBtn: qs("publishBtn"),
+    publishStatus: qs("publishStatus"),
+
+    collectionTitle: qs("collectionTitle"),
+    collectionType: qs("collectionType"),
+    collectionDescription: qs("collectionDescription"),
+    collectionYear: qs("collectionYear"),
+    createCollectionBtn: qs("createCollectionBtn"),
+    createCollectionStatus: qs("createCollectionStatus"),
+
+    collectionStats: qs("collectionStats"),
+
+    successText: qs("successText"),
+    viewGalleryBtn: qs("viewGalleryBtn"),
+    newUploadBtn: qs("newUploadBtn"),
+
+    collectionSuccessText: qs("collectionSuccessText"),
+    uploadToNewCollectionBtn: qs("uploadToNewCollectionBtn"),
+    backToDashboardBtn: qs("backToDashboardBtn"),
+
+    managerTitle: qs("managerTitle"),
+    managerMeta: qs("managerMeta"),
+    managerPhotos: qs("managerPhotos"),
+    managerAddPhotos: qs("managerAddPhotos"),
+    managerOpenGallery: qs("managerOpenGallery"),
+    managerBack: qs("managerBack")
+};
