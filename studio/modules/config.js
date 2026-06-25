64867ee3e30ccaf0dd7dd3165b73241b6d5583d4
mlopezmad <65028173+mlopezmad@users.noreply.github.com>
2026-06-25 16:09:59 +0200

    Crear config.js

---
+++ studio/modules/config.js
@@ -0,0 +1,10 @@
+export const WORKER_URL = "https://mlopezmad-estudio.mlopezmad.workers.dev";
+
+export const PROTECTED_COLLECTIONS = [
+    "hall-of-fame",
+    "madrid",
+    "middelburg",
+    "rotterdam",
+    "iphone4s-cadiz",
+    "iphone4s-caceres"
+];
